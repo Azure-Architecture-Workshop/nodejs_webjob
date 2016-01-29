c17a34dbbe3a3615c86b1eab73f66ab01f68307f
Sidney Andrews <sidney@seesharprun.net>
2016-01-28 23:34:50 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.host = "[the URI value from the DocumentDB Keys blade on http://portal.azure.com]";
-config.authKey = "[the PRIMARY KEY value from the DocumentDB Keys blade on http://portal.azure.com]";
+config.host = process.env.DocumentDBURI || "[the URI value from the DocumentDB Keys blade on http://portal.azure.com]";
+config.authKey = process.env.DocumentDBAuthKey || "[the PRIMARY KEY value from the DocumentDB Keys blade on http://portal.azure.com]";
 config.databaseId = "EntryList";
 config.collectionId = "Items";
 
